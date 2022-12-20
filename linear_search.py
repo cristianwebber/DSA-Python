7e7d88c18a15543c7526b573eecf191bf65e51dc
Cristian Webber <cristian.webber@hotmail.com>
2022-12-20 10:02:13 -0300

    feat: add linear_search

---
+++ linear_search.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import pytest
+
+
+def linear_search(haystack: list[int], needle: int) -> bool:
+    for i in range(0, len(haystack), 1):
+        if haystack[i] == needle:
+            return True
+    return False
+
+
+@pytest.mark.parametrize(
+    ('haystack', 'needle', 'expected'),
+    (
+        ([1, 2, 3], 2, True),
+        ([1, 2, 3], 4, False),
+        ([1, 2, 5], 5, True),
+        ([1, 2, 4], 3, False),
+    ),
+)
+def test(haystack: list[int], needle: int, expected: bool) -> None:
+    assert linear_search(haystack, needle) == expected

6101ce38e99aca13cc219b5dbcb12126a607303c
Cristian Webber <cristian.webber@hotmail.com>
2023-01-11 09:42:14 -0300

    perf(deque): add pop performace test

--- perf_deque_vs_list.py
+++ perf_deque_vs_list.py
@@ -1,13 +1,8 @@
-# time_append.py
 from __future__ import annotations
 
 from collections import deque
 from time import perf_counter
 from typing import Deque
-
-TIMES = 100_000
-a_list: list[int] = []
-a_deque: Deque[int] = deque()
 
 
 def average_time(func, times):
@@ -19,9 +14,29 @@ def average_time(func, times):
     return total / times
 
 
+# Perf insert left
+TIMES = 100_000
+a_list: list[int] = []
+a_deque: Deque[int] = deque()
+
 list_time = average_time(lambda i: a_list.insert(0, i), TIMES)
 deque_time = average_time(lambda i: a_deque.appendleft(i), TIMES)
 gain = list_time / deque_time
 
+print('Insert left:')
 print(f'list.insert()      {list_time:.6} ns')
 print(f'deque.appendleft() {deque_time:.6} ns  ({gain:.6}x faster)')
+
+
+# Perf pop
+TIMES = 100_000
+b_list: list[int] = [1] * TIMES
+b_deque: Deque[int] = deque(a_list)
+
+list_time = average_time(lambda i: b_list.pop(0), TIMES)
+deque_time = average_time(lambda i: b_deque.popleft(), TIMES)
+gain = list_time / deque_time
+
+print('Pop left:')
+print(f'list.pop(0)     {list_time:.6} ns')
+print(f'deque.popleft() {deque_time:.6} ns  ({gain:.6}x faster)')

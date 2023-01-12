45e4228ecf5a1156c838859a1f44776631434a7a
Cristian Webber <cristian.webber@hotmail.com>
2023-01-12 09:14:17 -0300

    perf(deque): add insert in middle perf

--- perf_deque_vs_list.py
+++ perf_deque_vs_list.py
@@ -31,7 +31,7 @@ print(f'deque.appendleft() {deque_time:.6} ns  ({gain:.6}x faster)')
 # Perf pop
 TIMES = 100_000
 b_list: list[int] = [1] * TIMES
-b_deque: Deque[int] = deque(a_list)
+b_deque: Deque[int] = deque(b_list)
 
 list_time = average_time(lambda i: b_list.pop(0), TIMES)
 deque_time = average_time(lambda i: b_deque.popleft(), TIMES)
@@ -40,3 +40,25 @@ gain = list_time / deque_time
 print('Pop left:')
 print(f'list.pop(0)     {list_time:.6} ns')
 print(f'deque.popleft() {deque_time:.6} ns  ({gain:.6}x faster)')
+
+
+# Perf insert in middle
+def time_it(sequence):
+    middle = len(sequence) // 2
+    sequence.insert(middle, 'middle')
+    sequence[middle]
+    sequence.remove('middle')
+    del sequence[middle]
+
+
+TIMES = 30_000
+c_list = [1] * TIMES
+c_deque = deque(c_list)
+
+list_time = average_time(lambda i: time_it(c_list), TIMES)
+deque_time = average_time(lambda i: time_it(c_deque), TIMES)
+gain = deque_time / list_time
+
+print('Insert in middle:')
+print(f'list  {list_time:.6} μs ({gain:.6}x faster)')
+print(f'deque {deque_time:.6} μs')

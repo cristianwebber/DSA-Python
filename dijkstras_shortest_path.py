17f9003f0ed4844d10225ff063d3a1893ec52814
Cristian Webber <cristian.webber@hotmail.com>
2023-02-14 09:38:00 -0300

    feat(graph): implement dijkstras_shortest_path

--- dijkstras_shortest_path.py
+++ dijkstras_shortest_path.py
@@ -5,41 +5,36 @@ import sys
 
 
 class Vertex:
-    def __init__(self, node):
-        self.id = node
-        self.adjacent = {}
+    def __init__(self, node: str):
+        self.id: str = node
+        self.adjacent: dict[str, int] = {}
         self.distance = sys.maxsize
         self.visited = False
-        self.previous = None
+        self.previous: None | str = None
 
-    def add_neighbor(self, neighbor, weight=0):
+    def add_neighbor(self, neighbor: str, weight: int = 0):
         self.adjacent[neighbor] = weight
 
-    def get_connections(self):
-        return self.adjacent.keys()
+    @property
+    def connections(self) -> list[str]:
+        return list(self.adjacent.keys())
 
-    def get_id(self):
-        return self.id
-
-    def get_weight(self, neighbor):
+    def get_weight(self, neighbor: str) -> int:
         return self.adjacent[neighbor]
 
-    def set_distance(self, dist):
+    def set_distance(self, dist: int) -> None:
         self.distance = dist
 
-    def get_distance(self):
+    def get_distance(self) -> int:
         return self.distance
 
-    def set_previous(self, prev):
+    def set_previous(self, prev: str) -> None:
         self.previous = prev
 
-    def set_visited(self):
+    def set_visited(self) -> None:
         self.visited = True
 
-    def __str__(self):
-        return str(self.id) + 'adjacent: ' + str([x.id for x in self.adjacent])
-
-    def __lt__(self, other):
+    def __lt__(self, other: str) -> bool:
         return id(self) > id(other)
 
 
@@ -51,19 +46,19 @@ class Graph:
     def __iter__(self):
         return iter(self.vert_dict.values())
 
-    def add_vertex(self, node):
+    def add_vertex(self, node: str) -> Vertex:
         self.num_vertices = self.num_vertices + 1
         new_vertex = Vertex(node)
         self.vert_dict[node] = new_vertex
         return new_vertex
 
-    def get_vertex(self, n):
+    def get_vertex(self, n: str) -> Vertex | None:
         if n in self.vert_dict:
             return self.vert_dict[n]
         else:
             return None
 
-    def add_edge(self, frm, to, cost=0):
+    def add_edge(self, frm: str, to: str, cost: int = 0):
         if frm not in self.vert_dict:
             self.add_vertex(frm)
         if to not in self.vert_dict:
@@ -72,41 +67,39 @@ class Graph:
         self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
         self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
 
-    def get_vertices(self):
-        return self.vert_dict.keys()
+    def get_vertices(self) -> list[str]:
+        return list(self.vert_dict.keys())
 
-    def set_previous(self, current):
+    def set_previous(self, current: str):
         self.previous = current
 
-    def get_previous(self, current):
+    def get_previous(self) -> str:
         return self.previous
 
 
 def shortest(v, path):
     ''' make shortest path from v.previous'''
     if v.previous:
-        path.append(v.previous.get_id())
+        path.append(v.previous.id)
         shortest(v.previous, path)
     return
 
 
-def dijkstra(aGraph, start, target):
-    # Set the distance for the start node to zero
-    start.set_distance(0)
+def dijkstra(graph: Graph, start: Vertex | None) -> None:
+    if start is None:
+        raise Exception("Start can't be a empty vertex")
+    else:
+        start.set_distance(0)
 
-    # Put tuple pair into the priority queue
-    unvisited_queue = [(v.get_distance(), v) for v in aGraph]
+    unvisited_queue = [(v.get_distance(), v) for v in graph]
     heapq.heapify(unvisited_queue)
 
     while len(unvisited_queue):
-        # Pops a vertex with the smallest distance
         uv = heapq.heappop(unvisited_queue)
         current = uv[1]
         current.set_visited()
 
-        # for next in v.adjacent:
         for next in current.adjacent:
-            # if visited, skip
             if next.visited:
                 continue
             new_dist = current.get_distance() + current.get_weight(next)
@@ -114,15 +107,6 @@ def dijkstra(aGraph, start, target):
             if new_dist < next.get_distance():
                 next.set_distance(new_dist)
                 next.set_previous(current)
-                print(
-                    'updated: current = %s next = %s new_dist = %s'
-                    % (current.get_id(), next.get_id(), next.get_distance()),
-                )
-            else:
-                print(
-                    'not updated: current = %s next = %s new_dist = %s'
-                    % (current.get_id(), next.get_id(), next.get_distance()),
-                )
 
         # Rebuild heap
         # 1. Pop every item
@@ -131,41 +115,41 @@ def dijkstra(aGraph, start, target):
         # 2. Put all vertices not visited into the queue
         unvisited_queue = [
             (v.get_distance(), v)
-            for v in aGraph if not v.visited
+            for v in graph if not v.visited
         ]
         heapq.heapify(unvisited_queue)
 
 
-if __name__ == '__main__':
+def test_dijkstra():
+    graph = Graph()
 
-    g = Graph()
+    graph.add_vertex('a')
+    graph.add_vertex('b')
+    graph.add_vertex('c')
+    graph.add_vertex('d')
+    graph.add_vertex('e')
+    graph.add_vertex('f')
 
-    g.add_vertex('a')
-    g.add_vertex('b')
-    g.add_vertex('c')
-    g.add_vertex('d')
-    g.add_vertex('e')
-    g.add_vertex('f')
+    graph.add_edge('a', 'b', 7)
+    graph.add_edge('a', 'c', 9)
+    graph.add_edge('a', 'f', 14)
+    graph.add_edge('b', 'c', 10)
+    graph.add_edge('b', 'd', 15)
+    graph.add_edge('c', 'd', 11)
+    graph.add_edge('c', 'f', 2)
+    graph.add_edge('d', 'e', 6)
+    graph.add_edge('e', 'f', 9)
 
-    g.add_edge('a', 'b', 7)
-    g.add_edge('a', 'c', 9)
-    g.add_edge('a', 'f', 14)
-    g.add_edge('b', 'c', 10)
-    g.add_edge('b', 'd', 15)
-    g.add_edge('c', 'd', 11)
-    g.add_edge('c', 'f', 2)
-    g.add_edge('d', 'e', 6)
-    g.add_edge('e', 'f', 9)
+    dijkstra(
+        graph=graph,
+        start=graph.get_vertex('a'),
+    )
 
-    for v in g:
-        for w in v.get_connections():
-            vid = v.get_id()
-            wid = w.get_id()
-            print('( %s , %s, %3d)' % (vid, wid, v.get_weight(w)))
-
-    dijkstra(g, g.get_vertex('a'), g.get_vertex('e'))
-
-    target = g.get_vertex('e')
-    path = [target.get_id()]
+    target = graph.get_vertex('e')
+    if target is None:
+        raise Exception("End can't be a empty vertex")
+    path = [target.id]
     shortest(target, path)
-    print('The shortest path: %s' % (path[::-1]))
+
+    path = path[::-1]
+    assert path == ['a', 'c', 'f', 'e']
